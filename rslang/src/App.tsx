ccbbbdea0c6c9e1b2a16bca41f92d93d4f6d8c80
LexSava <lexsava@gmail.com>
2021-03-22 18:34:47 +0300

    feat: add Loading screen
    Add Loading screen

--- rslang/src/App.tsx
+++ rslang/src/App.tsx
@@ -2,9 +2,24 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.scss";
 import React, { useState, useEffect } from "react";
 import HomePage from "./components/HomePage/HomePage";
+import LoadingScreen from "./components/LoadingScreen/LoadingScreen";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 function App() {
+  const [loading, setLoading] = useState(false);
+
+  setTimeout(() => {
+    setLoading(true);
+  }, 5000);
+
+  if (!loading) {
+    return (
+      <div className="App">
+        <LoadingScreen />
+      </div>
+    );
+  }
+
   return (
     <BrowserRouter>
       <div className="App">
