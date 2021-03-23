7eb36d0db4828caa7a7607d4d6437bee84da955e
LexSava <lexsava@gmail.com>
2021-03-23 14:03:10 +0300

    feat: add LoadableComponent
    Add LoadableComponent

--- rslang/src/App.tsx
+++ rslang/src/App.tsx
@@ -1,31 +1,38 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.scss";
 import React, { useState, useEffect } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Loadable from "react-loadable";
 import HomePage from "./components/HomePage/HomePage";
 import LoadingScreen from "./components/LoadingScreen/LoadingScreen";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 function App() {
   const [loading, setLoading] = useState(false);
 
-  setTimeout(() => {
-    setLoading(true);
-  }, 5000);
+  const LoadableComponent = Loadable({
+    loader: () => import("./components/HomePage/HomePage"),
+    loading: LoadingScreen,
+    delay: 5300,
+  });
 
-  if (!loading) {
-    return (
-      <div className="App">
-        <LoadingScreen />
-      </div>
-    );
-  }
+  // setTimeout(() => {
+  //   setLoading(true);
+  // }, 5000);
+
+  // if (!loading) {
+  //   return (
+  //     <div className="App">
+  //       <LoadingScreen />
+  //     </div>
+  //   );
+  // }
 
   return (
     <BrowserRouter>
       <div className="App">
         <Switch>
           <React.Fragment>
-            <Route path="/" component={HomePage} />
+            <Route path="/" component={LoadableComponent} />
           </React.Fragment>
         </Switch>
       </div>
