df5d7efc025ebf876688b7e727742cb6980c4704
LexSava <lexsava@gmail.com>
2021-03-24 14:10:48 +0300

    fix: add Route in main
    add Route in main

--- rslang/src/components/Main/Main.tsx
+++ rslang/src/components/Main/Main.tsx
@@ -1,11 +1,18 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./Main.scss";
 import React from "react";
+import { Container } from "react-bootstrap";
 import Study from "../Study/Study";
-
+import Vocabulary from "../Vocabulary/Vocabulary";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 interface InterfaceMain {}
 
 const Main: React.FC<InterfaceMain> = (props) => {
-  return <Study />;
+  return (
+    <Switch>
+      <Route exact path="/study" component={Study} />
+      <Route path="/vocabulary" component={Vocabulary} />
+    </Switch>
+  );
 };
 export default Main;
