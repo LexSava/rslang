16444580c978fce9d856373a1b544c69b1a4673b
LexSava <lexsava@gmail.com>
2021-03-25 00:05:54 +0300

    feat: add all components for the page TutorialPage
    Add all components for the page TutorialPage

--- rslang/src/components/Main/Main.tsx
+++ rslang/src/components/Main/Main.tsx
@@ -4,16 +4,22 @@ import React from "react";
 import { Container } from "react-bootstrap";
 import Study from "../Study/Study";
 import Vocabulary from "../Vocabulary/Vocabulary";
+import Games from "../Games/Games";
+import Statistics from "../Statistics/Statistics";
+import Settings from "../Settings/Settings";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 interface InterfaceMain {}
 
 const Main: React.FC<InterfaceMain> = (props) => {
   return (
-    <Study />
-    // <Switch>
-    //   <Route exact path="/study" component={Study} />
-    //   <Route path="/vocabulary" component={Vocabulary} />
-    // </Switch>
+    // <Study />
+    <Switch>
+      <Route exact path="/tutorial-page" component={Study} />
+      <Route path="/tutorial-page/vocabulary" component={Vocabulary} />
+      <Route path="/tutorial-page/games" component={Games} />
+      <Route path="/tutorial-page/statistics" component={Statistics} />
+      <Route path="/tutorial-page/settings" component={Settings} />
+    </Switch>
   );
 };
 export default Main;
