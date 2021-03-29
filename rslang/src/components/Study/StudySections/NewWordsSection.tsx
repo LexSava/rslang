3538dff6faa36a3cc693d4ef6caffc687c55d3e8
LexSava <lexsava@gmail.com>
2021-03-29 20:04:22 +0300

    feat: completed the functionality of the block for learning new words
    Completed the functionality of the block for learning new words

--- rslang/src/components/Study/StudySections/NewWordsSection.tsx
+++ rslang/src/components/Study/StudySections/NewWordsSection.tsx
@@ -2,6 +2,12 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./StudySections.scss";
 import React, { useState, useEffect } from "react";
 import {
+  BsSkipEndFill,
+  BsArrowDown,
+  BsArrowUp,
+  BsArrowRight,
+} from "react-icons/bs";
+import {
   Container,
   Button,
   Card,
@@ -11,6 +17,7 @@ import {
   FormControl,
   Jumbotron,
   Form,
+  ProgressBar,
 } from "react-bootstrap";
 const url = `https://serene-falls-78086.herokuapp.com/`;
 
@@ -40,13 +47,38 @@ const NewWordsSection: React.FC<InterfaceNewWordsSection> = (props) => {
   const [newWords, setNewWord] = useState<any>(props.words);
   const [wordCard, setWordCard] = useState<any>(0);
   const [show, setShow] = useState(false);
+  const [inputText, setInputText] = useState<string>("");
+  const [testButtonActivity, setTestButtonActivity] = useState<boolean>(true);
+  const [testButtonText, setTestButtonText] = useState<string>("Проверить");
+  const [testButtonArrow, setTestButtonArrow] = useState<any>(BsArrowUp);
+  const [hintButtonActivity, setHintButtonActivity] = useState<boolean>(false);
+
+  let [progressPercentage, setProgressPercentage] = useState<number>(0);
   let [cardNumber, setCardNumber] = useState<number>(0);
 
   useEffect(() => {
     setNewWord(props.words);
   }, [props.words]);
 
-  const playAudioWord = (volume = 0.2) => {
+  useEffect(() => {
+    setTestButtonText("Проверить");
+    setTestButtonArrow(BsArrowUp);
+  }, [cardNumber]);
+
+  useEffect(() => {
+    console.log(inputText);
+    if (inputText.length !== 0) {
+      setTestButtonActivity(false);
+    } else {
+      setTestButtonActivity(true);
+    }
+  }, [inputText]);
+
+  const enteredWord = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputText(event.target.value);
+  };
+
+  const playAudio = () => {
     const audioWord = new Audio(url + newWords[cardNumber].audio);
     const audioMeaning = new Audio(url + newWords[cardNumber].audioMeaning);
     const audioExample = new Audio(url + newWords[cardNumber].audioExample);
@@ -62,65 +94,102 @@ const NewWordsSection: React.FC<InterfaceNewWordsSection> = (props) => {
     // sound.preload = "auto";
     return audioWord.play();
   };
+  const playAudioWord = () => {
+    const audioWord = new Audio(url + newWords[cardNumber].audio);
+    audioWord.play();
+  };
 
-  const playAudio = (volume: number = 0.2) => {
-    let audioElements = [
-      new Audio(newWords[cardNumber].audio),
-      new Audio(newWords[cardNumber].audioMeaning),
-      new Audio(newWords[cardNumber].textExample),
-    ];
+  const wordCheck = () => {
+    if (inputText.toLowerCase() === newWords[cardNumber].word.toLowerCase()) {
+      setTestButtonText("Следующее слово");
+      setTestButtonArrow(BsArrowRight);
+      setProgressPercentage(5 + progressPercentage);
+    } else {
+      setTestButtonText("Проверить");
+      setTestButtonArrow(BsArrowUp);
+    }
+  };
 
-    for (let i = 0; i < audioElements.length; i++) {
-      if (i === 0) {
-        // Первое аудио запускаем
-        // audioElements[i].volume = volume;
-        audioElements[i].play();
-      } else {
-        // Остальные — после окончания предыдущего
-        audioElements[i - 1].addEventListener("ended", function () {
-          audioElements[i].play();
-        });
-      }
+  const keyPressHandler = (event: React.KeyboardEvent) => {
+    if (event.key === "Enter") {
+      wordCheck();
     }
   };
 
   const showNextCard = () => {
-    if (cardNumber < newWords.length - 1) {
-      setCardNumber(++cardNumber);
+    if (cardNumber <= newWords.length && testButtonText === "Следующее слово") {
+      setCardNumber(1 + cardNumber);
       setShow(false);
-    } else {
+      setInputText("");
+      setHintButtonActivity(false);
+    } else if (cardNumber === 19 && testButtonText === "Следующее слово") {
       setCardNumber(0);
       setShow(false);
+      setProgressPercentage(0);
+    } else {
     }
+  };
+  const getHint = () => {
+    setInputText(newWords[cardNumber].word);
+    setHintButtonActivity(true);
+    playAudioWord();
+    console.log(testButtonText);
   };
 
   useEffect(() => {
-    console.log(newWords);
     setWordCard(
-      <Card style={{ width: "25rem" }} key={newWords[cardNumber].id}>
+      <Card
+        style={{ width: "30rem" }}
+        key={newWords[cardNumber].id}
+        className="card-new-words"
+      >
         <Card.Img variant="top" src={url + newWords[cardNumber].image} />
         <Card.Body>
-          <Card.Title style={show ? { display: "block" } : { display: "none" }}>
+          <Card.Title
+            style={show ? { display: "block" } : { display: "none" }}
+            className="text-primary font-weight-bold"
+          >
             {newWords[cardNumber].word}
+            <Button
+              variant="outline-info"
+              className="ml-5"
+              style={{ float: "right" }}
+              onClick={() => {
+                playAudio();
+              }}
+            >
+              Прослушать <BsSkipEndFill />
+            </Button>
           </Card.Title>
-          <Card.Title style={show ? { display: "block" } : { display: "none" }}>
+          <Card.Title
+            style={show ? { display: "block" } : { display: "none" }}
+            className="font-weight-light"
+          >
             {newWords[cardNumber].transcription}
           </Card.Title>
-          <Card.Title>{newWords[cardNumber].wordTranslate}</Card.Title>
+          <Card.Title className="font-weight-bold">
+            {newWords[cardNumber].wordTranslate}
+          </Card.Title>
         </Card.Body>
         <ListGroup className="list-group-flush">
           <ListGroupItem>
             {" "}
-            <Card.Text>{newWords[cardNumber].textMeaning}</Card.Text>
+            <Card.Text className="h6">
+              {newWords[cardNumber].textMeaning}
+            </Card.Text>
             <Card.Text
+              className="blockquote-footer"
               style={show ? { display: "block" } : { display: "none" }}
             >
               {newWords[cardNumber].textMeaningTranslate}
             </Card.Text>
           </ListGroupItem>
           <ListGroupItem>
-            <Card.Text>{newWords[cardNumber].textExample}</Card.Text>
+            <Card.Text className="h6">
+              {newWords[cardNumber].textExample}
+            </Card.Text>
             <Card.Text
+              className="blockquote-footer"
               style={show ? { display: "block" } : { display: "none" }}
             >
               {newWords[cardNumber].textExampleTranslate}
@@ -128,33 +197,64 @@ const NewWordsSection: React.FC<InterfaceNewWordsSection> = (props) => {
           </ListGroupItem>
         </ListGroup>
         <Card.Body className="">
-          <Form.Control type="text" placeholder="Normal text" />
+          <Form.Control
+            type="text"
+            placeholder="Введите слово"
+            value={inputText}
+            onChange={enteredWord}
+            onInput={enteredWord}
+            onKeyPress={keyPressHandler}
+            autoFocus
+            className="mb-3"
+          />
           {/* <Card.Link href="#">Card Link</Card.Link>
           <Card.Link href="#">Another Link</Card.Link> */}
-          <Button
-            variant="primary"
-            onClick={() => {
-              showNextCard();
-              playAudioWord();
-            }}
-          >
-            Проверить
-          </Button>
-          <Button
-            variant="primary"
-            onClick={() => {
-              setShow(!show);
-              playAudioWord();
-            }}
-          >
-            Показать
-          </Button>
+          <Container className="d-flex justify-content-around">
+            <Button
+              id="check-button"
+              variant="success"
+              disabled={testButtonActivity}
+              onClick={() => {
+                showNextCard();
+                //   playAudioWord();
+                wordCheck();
+              }}
+            >
+              {testButtonText}
+              {testButtonArrow}
+            </Button>
+            <Button
+              className="ml-3"
+              variant="outline-info"
+              disabled={hintButtonActivity}
+              onClick={() => {
+                setShow(!show);
+                // playAudio();
+                getHint();
+              }}
+            >
+              Показать ответ
+              <BsArrowDown />
+            </Button>
+          </Container>
         </Card.Body>
       </Card>
     );
-  }, [newWords, show, cardNumber]);
+  }, [
+    newWords,
+    show,
+    cardNumber,
+    testButtonActivity,
+    inputText,
+    testButtonText,
+  ]);
 
   //   showWordCard();
-  return <Jumbotron className="bg-light mt-4 min-vh-100">{wordCard}</Jumbotron>;
+  return (
+    <Jumbotron className="bg-light min-vh-100">
+      <ProgressBar now={progressPercentage} label={`${progressPercentage}%`} />
+      {wordCard}
+    </Jumbotron>
+  );
 };
 export default NewWordsSection;
