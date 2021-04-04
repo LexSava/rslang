c42e1eac101bb346656c76a8a854b5598d72c4c8
dzmitryNz <d.nayezzhy@gmail.com>
2021-04-04 21:24:12 +0300

    feat: avatar add

--- rslang/src/components/TutorialPage/TutorialPage.tsx
+++ rslang/src/components/TutorialPage/TutorialPage.tsx
@@ -3,12 +3,16 @@ import "./TutorialPage.scss";
 import React from "react";
 import { Container } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { url } from "../../api/defData";
 import Menu from "../Menu/Menu";
 import Main from "../Main/Main";
 import SignOut from "../Signin/SignOut";
 interface InterfaceTutorialPage {}
 
 const TutorialPage: React.FC<InterfaceTutorialPage> = (props) => {
+  const avatar:string | null = localStorage.getItem("userpic") || "";
+  const avatarUrl = `${url}${JSON.parse(avatar)}`;
+
   return (
     <Container className="min-vh-100">
       <Container className="bg-warning p-3 d-flex justify-content-between">
@@ -16,6 +20,7 @@ const TutorialPage: React.FC<InterfaceTutorialPage> = (props) => {
           <h1 className="logo-tutorial-page text-dark m-0">RS Lang</h1>
         </Link>
         <SignOut />
+        {avatar && (<img className="avatar" src={avatarUrl} alt="avatar"/>)}
         <Link
           to="/"
           className="btn btn-primary rounded-pill border border-0 btn-lg mr-3 login-button"
