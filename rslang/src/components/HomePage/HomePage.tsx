8c1348984e4d482b3c7430a45f0e9efb2e9f0e45
Anastasia Ivanova <amastasia.ivanova@minsk.leveris.com>
2021-04-13 22:48:51 +0300

    docs: add developer description

--- rslang/src/components/HomePage/HomePage.tsx
+++ rslang/src/components/HomePage/HomePage.tsx
@@ -282,9 +282,8 @@ const HomePage: React.FC<InterfaceHomePage> = (props) => {
                       <small className="text-muted">Front-end Developer</small>
                     </p>
                     <p className="card-text">
-                      This is a wider card with supporting text below as a
-                      natural lead-in to additional content. This content is a
-                      little bit longer.
+                      Реализовала мини-игру "Аудиовызов", блок выбора уровня
+                      сложности, стартовый экран игры, результаты.
                     </p>
                   </div>
                 </div>
